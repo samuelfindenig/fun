3fe6685c41f6ae4ddc2bfc5d01a5157cad85dfe3
Samuel Findenig <146440803+samuelfindenig@users.noreply.github.com>
2024-07-18 14:33:30 +0200

    Added memo to Funktion fib()

--- fib.py
+++ fib.py
@@ -1,5 +1,8 @@
-def fib(nun):
-    if num <= 2:
+def fib(nun, memo = {}):
+    if num in memo:
+        return memo[num]
+    elif num <= 2:
         return 1
     else:
-        return fib(num-1)+fib(num-2)
+        memo[num] = fib(num-1, memo)+fib(num-2, memo)
+        return memo[num]
